cdebbc39f02324c32958cfab71ef9cc6542b9844
Daniel <dani3lkamanthi@gmail.com>
2024-07-07 19:10:31 +0300

    Completes code challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -24,6 +24,89 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class RestaurantResource(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        restaurants_list = [restaurant.to_dict(only=("id", "name", "address")) for restaurant in restaurants]
+        return make_response(jsonify(restaurants_list), 200)
+    
+class RestaurantByID(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if restaurant:
+            restaurant_pizzas = []
+            for rp in restaurant.restaurant_pizzas:
+                pizza_data = rp.pizza.to_dict(only=("id", "name", "ingredients"))
+                restaurant_pizza_data = {
+                    "id": rp.id,
+                    "restaurant_id": rp.restaurant_id,
+                    "pizza_id": rp.pizza_id,
+                    "price": rp.price,
+                    "pizza": pizza_data
+                }
+                restaurant_pizzas.append(restaurant_pizza_data)
+
+            restaurant_data = restaurant.to_dict(only=("id", "name", "address"))
+            restaurant_data["restaurant_pizzas"] = restaurant_pizzas
+            return make_response(jsonify(restaurant_data), 200)
+        
+        else:
+            return make_response(jsonify({"error": "Restaurant not found"}), 404)
+        
+class DeleteRestaurantByID(Resource):
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+
+        if restaurant:
+            for rp in restaurant.restaurant_pizzas:
+               db.session.delete(rp)
+            db.session.delete(restaurant)
+            db.session.commit()
+            return make_response(jsonify({"message": "Restaurant deleted"}), 204)
+        
+        else:
+            return make_response(jsonify({"error": "Restaurant not found"}), 404)
+        
+class PizzasResource(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        pizzas_list = [pizza.to_dict(only=("id", "name", "ingredients")) for pizza in pizzas]
+        return make_response(jsonify(pizzas_list), 200)
+    
+class CreateRestaurantPizza(Resource):
+    def post(self):
+        data = request.get_json()
+        pizza = Pizza.query.filter_by(id=data.get('pizza_id')).first()
+        restaurant = Restaurant.query.filter_by(id=data.get('restaurant_id')).first()
+
+        if pizza and restaurant:
+            price = data.get('price')
+            if price is None or price < 1 or price > 30:
+                return make_response(jsonify({"errors": ["validation errors"]}), 400)
+
+            restaurant_pizza = RestaurantPizza(price=price, pizza_id=data.get('pizza_id'), restaurant_id=data.get('restaurant_id'))
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+
+            restaurant_pizza_data = {
+                "id": restaurant_pizza.id,
+                "restaurant_id": restaurant_pizza.restaurant_id,
+                "pizza_id": restaurant_pizza.pizza_id,
+                "price": restaurant_pizza.price,
+                "pizza": pizza.to_dict(only=("id", "name", "ingredients")),
+                "restaurant": restaurant.to_dict(only=("id", "name", "address"))
+            }
+            return make_response(jsonify(restaurant_pizza_data), 201)
+
+        else:
+            return make_response(jsonify({"errors": ["Pizza or Restaurant not found."]}), 404)
+
+api.add_resource(RestaurantResource, '/restaurants')
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')
+api.add_resource(DeleteRestaurantByID, '/restaurants/<int:id>', methods=["DELETE"])
+api.add_resource(PizzasResource, '/pizzas')
+api.add_resource(CreateRestaurantPizza, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
